fd5c73c8bd680d8cfd5e1110fd785983d36a5d3e
Danilo Carlos Marques <danilo.marques@basis.com.br>
2021-12-06 23:59:41 -0300

    Conhecendo JSX, Carregando CSS, Primeiros componentes, propriedades dos componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import MeuComponente from "./components/basicos/Primeiro";
+import { ComProp } from "./components/basicos/ComProp";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <MeuComponente></MeuComponente>
+            <ComProp aluno="Pedro" materia="Matemática" nota={5.6}></ComProp>
+        </div>
+    );
 }
 
 export default App;
