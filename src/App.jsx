2cbac706b1b6bea30cbe17199a51b294804fce08
Danilo Carlos Marques <danilo.marques@basis.com.br>
2021-12-07 12:35:16 -0300

    Estilizacao do componente card com props

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,32 @@ import MeuComponente from "./components/basicos/Primeiro";
 import { ComProp } from "./components/basicos/ComProp";
 import { Aleatorio } from "./components/basicos/Aleatorio";
 import { Card } from "./components/Layout/Card";
+import "./styles/App.css";
 
 export default function App() {
     return (
-        //<div className="App">
-        <>
-            <Card titulo="#03 - Desafio de número Aleatório">
-                <Aleatorio min={1} max={100} />
-                <Aleatorio min={1} max={100} />
-            </Card>
+        <div className="App">
+            <div className="cards">
+                <Card
+                    titulo="#03 - Desafio de número Aleatório"
+                    color="#7cb5e9"
+                >
+                    <Aleatorio min={1} max={100} />
+                    <Aleatorio min={1} max={100} />
+                </Card>
 
-            <Card titulo="#02 - Componente com parâmetro">
-                <ComProp
-                    aluno="Pedro"
-                    materia="Matemática"
-                    nota={5.6}
-                ></ComProp>
-            </Card>
+                <Card titulo="#02 - Componente com parâmetro" color="#5fc189">
+                    <ComProp
+                        aluno="Pedro"
+                        materia="Matemática"
+                        nota={5.6}
+                    ></ComProp>
+                </Card>
 
-            <Card titulo="#01 - Primeiro Componente">
-                <MeuComponente></MeuComponente>
-            </Card>
-        </>
-        //</div>
+                <Card titulo="#01 - Primeiro Componente" color="#ea77af">
+                    <MeuComponente></MeuComponente>
+                </Card>
+            </div>
+        </div>
     );
 }
--- src/components/Layout/Card.jsx
+++ src/components/Layout/Card.jsx
@@ -1,8 +1,13 @@
 import "../../styles/Card.css";
 
 export function Card(props) {
+    const cardStyle = {
+        backgroundColor: props.color || "#f00",
+        borderColor: props.color || "#f00",
+    };
+
     return (
-        <div className="card">
+        <div className="card" style={cardStyle}>
             <div className="title">{props.titulo}</div>
             <div className="content">{props.children}</div>
         </div>
